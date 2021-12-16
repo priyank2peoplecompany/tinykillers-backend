8dcbebb8e788053b3007f5b61e9f16c6e3309502
priyanktwopeoplecompany <priyank@twopeople.company>
2021-12-16 13:00:06 +0530

    Update Quiz model

--- server/models/quiz/quiz.model.js
+++ server/models/quiz/quiz.model.js
@@ -3,14 +3,16 @@ const QuizSchema = new Schema({
         type: String,
         required: true,
     },
-    options: {
-        type: JSON,
-        required: true,
-    },
-    answer: {
-        type: String,
-        required: true,
-    }
+    options: [{
+        answer: {
+            type: String,
+            required: true,
+        },
+        point:{
+            type: Number,
+            required: true,
+        }
+    }],
 }, {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
     versionKey: false,
